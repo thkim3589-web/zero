79de5263d78a65d447c5f35aa33a4ae88603feaf
thkim3589-web <thkim3589@gmail.com>
2026-07-09 20:17:20 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,10 @@
 
-const CACHE='depth-v48';
-self.addEventListener('install',e=>self.skipWaiting());
+const CACHE='depth-v49';
+self.addEventListener('install',e=>{
+ self.skipWaiting();
+ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html'])));
+});
 self.addEventListener('activate',e=>clients.claim());
+self.addEventListener('fetch',e=>{
+ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
